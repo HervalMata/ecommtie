dc7449ebf24337ad440475dbde8fb75ac36b18c3
HervalMata <hervalmataamparo@gmail.com>
2020-07-04 17:48:22 -0300

    Criação do Projeto

---
+++ src/main/java/com/herval/ecommtie/EcommtieApplication.java
@@ -0,0 +1,13 @@
+package com.herval.ecommtie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommtieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommtieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herval/ecommtie/EcommtieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herval.ecommtie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommtieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
